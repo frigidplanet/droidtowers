5ce5249913c3a77f62ea54278fe4d5090512db0b
frigidplanet <jharris907@gmail.com>
2014-06-16 17:15:37 -0700

    remove all robotmedia classes and usage.

--- src/com/happydroids/droidtowers/DroidTowersGooglePlay.java
+++ src/com/happydroids/droidtowers/DroidTowersGooglePlay.java
@@ -4,15 +4,11 @@
 
 package com.happydroids.droidtowers;
 
-import net.robotmedia.billing.BillingController;
-import net.robotmedia.billing.BillingRequest;
-import net.robotmedia.billing.helper.AbstractBillingObserver;
 import android.util.DisplayMetrics;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.happydroids.HappyDroidConsts;
 import com.happydroids.droidtowers.gamestate.server.TowerGameService;
 import com.happydroids.droidtowers.platform.Display;
 import com.happydroids.platform.AndroidBrowserUtil;
@@ -21,12 +17,9 @@ import com.happydroids.platform.AndroidUncaughtExceptionHandler;
 import com.happydroids.platform.Platform;
 import com.happydroids.platform.PlatformConnectionMonitor;
 
-public class DroidTowersGooglePlay extends AndroidApplication implements
-		BillingController.IConfiguration {
+public class DroidTowersGooglePlay extends AndroidApplication {
 	private static final String TAG = DroidTowersGooglePlay.class.getSimpleName();
 	
-	private AbstractBillingObserver mBillingObserver;
-
 	public void onCreate(android.os.Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		DisplayMetrics metrics = new DisplayMetrics();
@@ -60,45 +53,4 @@ public class DroidTowersGooglePlay extends AndroidApplication implements
 		Gdx.input.setCatchMenuKey(true);
 	}
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		BillingController.unregisterObserver(mBillingObserver); // Avoid
-		// receiving
-		// notifications after
-		// destroy
-		BillingController.setConfiguration(null);
-	}
-
-	private void onRequestPurchaseResponse(String itemId,
-			BillingRequest.ResponseCode response) {
-
-	}
-
-	public BillingController.BillingStatus checkBillingSupported() {
-		return BillingController.checkBillingSupported(this);
-	}
-
-	public void requestPurchase(String itemId) {
-		BillingController.requestPurchase(this, itemId);
-	}
-
-	/**
-	 * Requests to restore all transactions.
-	 */
-	public void restoreTransactions() {
-		if (!mBillingObserver.isTransactionsRestored()) {
-			BillingController.restoreTransactions(this);
-		}
-	}
-
-	@Override
-	public byte[] getObfuscationSalt() {
-		return HappyDroidConsts.OBFUSCATION_SALT;
-	}
-
-	@Override
-	public String getPublicKey() {
-		return HappyDroidConsts.OBFUSCATION_KEY;
-	}
 }
