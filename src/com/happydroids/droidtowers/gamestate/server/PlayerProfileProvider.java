1283b69d423bfc8179d6be6394478f93fcfe49f4
kaltinril <jeremy.swartwood@gmail.com>
2014-06-19 20:50:29 -0800

    Removed 16 more classes that appear to not be used.  Verified by opening a tower save, checking options menu, etc.

--- src/com/happydroids/droidtowers/gamestate/server/PlayerProfileProvider.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2012. HappyDroids LLC, All rights reserved.
- */
-
-package com.happydroids.droidtowers.gamestate.server;
-
-public enum PlayerProfileProvider {
-	facebook, twitter
-}
